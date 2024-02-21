99ee0436cc773070aa963b5dc75fdce061d9f410
Ameer jafar <ameerjafar123@gmail.com>
2024-02-21 20:31:01 +0530

    adding code

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,22 @@
+import { PrismaClient } from '@prisma/client';
 import express from 'express';
 const app = express();
-
+const prisma = new PrismaClient();
+app.get('/posts/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const data = prisma.users.findFirst({
+        where: {
+            id
+        }
+    })
+    console.log(data.Post);
+})
+app.put('/posts/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const data = prisma.post.findFirst({
+        where: {
+            userId: id
+        }
+    });
+})
 app.listen(3000);
